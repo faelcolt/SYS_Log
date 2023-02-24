af311e851a2151d138c5ad2468a38a519eebc453
Rafael Cordeiro <fael_colt@hotmail.com>
2023-02-24 17:32:15 -0300

    att

--- Interface.py
+++ Interface.py
@@ -3,9 +3,10 @@ from PySimpleGUI import PySimpleGUI as sg
 # Layouts
 sg.theme('Reddit')
 layout = [
-    [],
-    [],
-    []
+    [sg.Text('Usuário'), sg.Input(key='usuario')],
+    [sg.Text('Senha'), sg.Input(key='senha', password_char='*')],
+    [sg.Checkbox('Salvar o Login?')]
+    [sg.Button('Entrar')]
 ]
 # Windows
 
