d666893aa046f8b6748918960acf9970ec4dca2b
Rafael Cordeiro <fael_colt@hotmail.com>
2023-02-24 20:26:04 -0300

    Finalizando o login simples

--- Interface.py
+++ Interface.py
@@ -5,10 +5,17 @@ sg.theme('Reddit')
 layout = [
     [sg.Text('Usuário'), sg.Input(key='usuario')],
     [sg.Text('Senha'), sg.Input(key='senha', password_char='*')],
-    [sg.Checkbox('Salvar o Login?')]
+    [sg.Checkbox('Salvar o Login?')],
     [sg.Button('Entrar')]
 ]
 # Windows
+janela = sg.Window('Tela de Login', layout)
 
 # Events
-
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        if valores['usuario'] == 'meuNome' and valores['senha'] == '12345':
+            print('Login efetuado com sucesso! ')
